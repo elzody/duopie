29c69a0fced69301435b9ff563f3a1e230c07a0d
Elizabeth Danzberger <spookystuff@protonmail.com>
2023-12-26 20:35:10 -0500

    added searchbar, usercard components

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { PhMagnifyingGlass } from '@phosphor-icons/vue';
+</script>
+
+<template>
+  <search class="search-bar">
+    <PhMagnifyingGlass class="search-bar-icon" :size="24" weight="duotone" />
+    <input class="search-bar-input" type="search" placeholder="Enter username" />
+  </search>
+</template>
+
+<style>
+.search-bar {
+  border: 2px solid var(--swan);
+  border-radius: 1rem;
+  background-color: var(--polar);
+  padding: 10px;
+  width: 400px;
+
+  & * {
+    vertical-align: middle;
+  }
+}
+
+.search-bar-icon {
+  color: var(--hare);
+}
+
+.search-bar-input {
+  margin-left: 10px;
+  background: transparent;
+  font-size: 1.2rem;
+  outline: 0;
+  border: 0;
+  font-family: 'Nunito', sans-serif;
+}
+</style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { PhFire } from '@phosphor-icons/vue';
+
+const data = ref({
+  image: "https://simg-ssl.duolingo.com/avatars/7699505/1iZJexKMq1/xlarge",
+  name: "elizabeth",
+  username: "Liza_72",
+  streak: 504,
+});
+</script>
+
+<template>
+  <article class="user-card">
+    <img class="user-card-img" :src="data.image" alt="profile picture" />
+    <div class="user-card-info">
+      <div class="user-card-name">{{ data.name }}</div>
+      <div class="user-card-username">{{ data.username }}</div>
+      <div class="user-card-streak">
+        <PhFire :size="24" weight="duotone" />
+        <span>{{ data.streak }}</span>
+      </div>
+    </div>
+  </article>
+</template>
+
+<style>
+.user-card {
+  display: flex;
+  border-radius: 1rem;
+  padding: 1rem;
+  width: fit-content;
+  height: 8rem;
+  box-shadow: 0 0 1rem var(--swan);
+}
+
+.user-card-img {
+  width: 8rem;
+  height: 8rem;
+  border-radius: 50%;
+  box-shadow: 0 0 1rem var(--swan);
+}
+
+.user-card-info {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  padding: 0 20px;
+}
+
+.user-card-name {
+  font-size: 1.5rem;
+}
+
+.user-card-username {
+  color: var(--eel);
+}
+
+.user-card-streak {
+  margin-top: 1rem;
+
+  & * {
+    vertical-align: middle;
+  }
+
+  & svg {
+    color: var(--fox);
+  }
+}
+</style>
